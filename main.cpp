c334d581f3af6f8f10f829bc5ff511e008047c78
riffgriff <griffinmartin02@gmail.com>
2023-12-03 13:22:31 -0500

    int maps and name key

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 #include <random>
 
 int main() {
-    std::ifstream file("C:\\Users\\bengs\\OneDrive\\Desktop\\WikiRace\\wikirace.csv");
+    std::ifstream file("C:\\Users\\griff\\OneDrive\\Desktop\\Fall 2023\\DSA\\Final project\\enwiki-2013.csv");
+    std::ifstream key("C:\\Users\\griff\\OneDrive\\Desktop\\Fall 2023\\DSA\\Final project\\enwiki-2013-names.csv");
 
     if (!file.is_open()) 
     {
@@ -15,25 +16,56 @@ int main() {
         return 1;
     }
 
-
     std::string line;
-    // now call respective files to build the data structures maybe we can just do one container but traverse differently
-    std::unordered_map<std::string, std::vector<std::string>> adjacencyList;
 
+    // now call respective files to build the data structures maybe we can just do one container but traverse differently
+    std::unordered_map<int, std::vector<int>> adjacencyList;
+
+    std::unordered_map<int, std::string> nameKey;
+
+    std::string fromNodeID, toNodeID;
+    bool first = true;
+
+    // read in adjacency list
     while (std::getline(file, line)) 
     {
         std::istringstream ss(line);
-        std::string fromNodeID, toNodeID;
-
+        
         // comma-separated file
         std::getline(ss, fromNodeID, ',');
         std::getline(ss, toNodeID);
 
-        // this adds the toNode to the fromNode key
-        adjacencyList[fromNodeID].push_back(toNodeID);
-    }
+        // this skips the csv header
+        if (first) {
+            first = false;
+            continue;
+        }
 
+        // this adds the toNode to the fromNode key
+        adjacencyList[std::stoi(fromNodeID)].push_back(std::stoi(toNodeID));
+    }
+    // max link list length 8104
     file.close();
+
+    //read in name map
+    first = true;
+    std::string nameId, nameString;
+    while (std::getline(key, line)) {
+
+        std::istringstream ss(line);
+        
+        // comma-separated file
+        std::getline(ss, nameId, ',');
+        std::getline(ss, nameString);
+
+        // this skips the csv header
+        if (first) {
+            first = false;
+            continue;
+        }
+
+        nameKey[std::stoi(nameId)] = nameString;
+    }
 
     // Random number generation setup
     std::random_device rd;
@@ -43,19 +75,21 @@ int main() {
     // Select two random different starting and ending URLs
     auto iter = adjacencyList.begin();
     std::advance(iter, distrib(gen));
-    std::string startingURL = iter->first;
+    int startingURL = iter->first;
 
-    std::string endingURL;
+    int endingURL;
     do {
         iter = adjacencyList.begin();
         std::advance(iter, distrib(gen));
         endingURL = iter->first;
     } while (startingURL == endingURL);
 
-    std::cout << startingURL << std::endl;
-    std::cout << endingURL << std::endl;
+    std::cout << nameKey[startingURL] << std::endl;
+    std::cout << nameKey[endingURL] << std::endl;
 
     // now call methods to traverse accordingly
+
+
 
     return 0;
 
