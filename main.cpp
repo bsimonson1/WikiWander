369e5917d572a24b23226c18cdcdc1d93763a555
riffgriff <griffinmartin02@gmail.com>
2023-12-03 15:23:11 -0500

    added BFS and DFS

--- main.cpp
+++ main.cpp
@@ -3,8 +3,105 @@
 #include <sstream>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <random>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <set>
+
+struct Node {
+    int id;
+    Node* parent;
+};
+
+std::vector<int> BFS_Search(int startPage, int endPage, std::unordered_map<int, std::vector<int>>& adjacencyList) {
+    std::unordered_set<int> visited;
+    std::queue<Node> toVisit;
+    std::vector<int> path;
+
+    Node firstNode;
+    firstNode.id = startPage;
+    firstNode.parent = nullptr;
+    toVisit.push(firstNode);
+
+    Node* currNode = &firstNode;
+    int iterations = 0;
+
+    while (currNode->id != endPage) {
+        // go through the adj list of the current node
+        for (int i = 0; i < adjacencyList[currNode->id].size(); i++) {
+            //check if it is already in set
+            int currId = adjacencyList[currNode->id][i];
+            auto it = visited.find(currId);
+            if (it == visited.end()) {
+                Node newNode;
+                newNode.id = adjacencyList[currNode->id][i];
+                newNode.parent = currNode;
+                toVisit.push(newNode);
+                
+            }
+        }
+        visited.insert(currNode->id);
+        currNode = &toVisit.front();
+        toVisit.pop();
+        iterations++;
+        std::cout << "Iteration: " << iterations << "\tVisiting: " << currNode->id << "\tTo visit size: " << toVisit.size() << "\tVisited size: " << visited.size() << std::endl;
+    }
+
+    std::stack<int> history;
+    // go back through the history
+    while (currNode != nullptr) {
+        history.push(currNode->id);
+        currNode = currNode->parent;
+    }
+    // reverse the order
+    while (history.size() != 0) {
+        path.push_back(history.top());
+        history.pop();
+    }
+
+    return path;
+    
+}
+
+std::set<int> DepthFirstTraversal(std::unordered_map<int, std::vector<int>>& adjacencyList, const int startingURL, const int endingURL) {
+    std::set<int> visited;
+    std::stack<int> s;
+    s.push(startingURL);
+
+    while (!s.empty()) 
+    {
+        int u = s.top();
+        s.pop();
+
+        // Check if this node has been visited
+        if (visited.find(u) != visited.end()) continue;
+
+        // Visit this node
+        visited.insert(u);
+
+        // Check if this is the ending URL
+        if (u == endingURL) 
+        {
+            return visited;
+        }
+
+        // Iterate over neighbors
+        for (const auto& v : adjacencyList[u]) 
+        {
+            if (visited.find(v) == visited.end()) 
+            {
+                s.push(v);
+            }
+        }
+    }
+
+    return visited; // If the ending URL is not found
+}
+
+
 
 int main() {
     std::ifstream file("C:\\Users\\griff\\OneDrive\\Desktop\\Fall 2023\\DSA\\Final project\\enwiki-2013.csv");
@@ -89,6 +186,19 @@ int main() {
 
     // now call methods to traverse accordingly
 
+    // BFS traversal:
+    std::vector<int> BFSpath;
+
+    //TEST from 7 to 6 - 7->5->6
+    BFSpath = BFS_Search(21, 18, adjacencyList);
+
+    std::cout << "BFS path: ";
+    for (int i = 0; i < BFSpath.size(); i++) {
+        std::cout << BFSpath[i] << ", ";
+    }
+
+    //DFS
+    std::cout << DepthFirstTraversal(adjacencyList, 7, 6).size() - 1 << std::endl;
 
 
     return 0;
@@ -108,3 +218,5 @@ int main() {
     //     std::cout << std::endl;
     // }
 }
+
+
