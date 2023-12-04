b93c645caba91896bed3cc44312040c67bf9e69b
Ben <100646537+bsimonson1@users.noreply.github.com>
2023-12-04 12:06:40 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -81,25 +81,22 @@ std::set<int> DepthFirstTraversal(std::unordered_map<int, std::vector<int>>& adj
 
     while (!s.empty()) 
     {
+        // pop node from stack to check
         int u = s.top();
         s.pop();
-
-        // Check if this node has been visited
+        //check if already visited
         if (visited.find(u) != visited.end())
         {
             continue;
         } 
-
-        // Visit this node
+        // if not visited, insert it to the visisted container
         visited.insert(u);
-
-        // Check if this is the ending URL
+        // check complete condition
         if (u == endingURL) 
         {
             return visited;
         }
-
-        // Iterate over neighbors
+        // if not complete and not visisted, iterate through the nodes values and explore the first one
         for (const auto& v : adjacencyList[u]) 
         {
             if (visited.find(v) == visited.end()) 
@@ -109,7 +106,7 @@ std::set<int> DepthFirstTraversal(std::unordered_map<int, std::vector<int>>& adj
         }
     }
 
-    return visited; // If the ending URL is not found
+    return visited; 
 }
 
 
@@ -124,12 +121,15 @@ int main(int argc, char* argv[])
     std::string start_url = argv[1];
     std::string end_url = argv[2];
 
-    int startingURL = std::stoi(start_url);
-    int endingURL = std::stoi(end_url);
+    // int startingURL = std::stoi(start_url);
+    // int endingURL = std::stoi(end_url);
 // int main()
 // {
-//     int startingURL = 7;
-//     int endingURL = 200;
+//     // int startingURL = 7;
+//     // int endingURL = 200;
+//     std::string start_url = "Talageh";
+//     std::string end_url = "Talageh-ye Sofla, Khuzestan";
+
     std::ifstream file("C:\\Users\\bengs\\OneDrive\\Desktop\\WikiWander\\sites.csv");
     std::ifstream key("C:\\Users\\bengs\\OneDrive\\Desktop\\WikiWander\\enwiki-2013-names.csv");
 
@@ -144,7 +144,7 @@ int main(int argc, char* argv[])
     // now call respective files to build the data structures maybe we can just do one container but traverse differently
     std::unordered_map<int, std::vector<int>> adjacencyList;
 
-    std::unordered_map<int, std::string> nameKey;
+    //std::unordered_map<int, std::string> nameKey;
 
     std::string fromNodeID, toNodeID;
     bool first = true;
@@ -171,6 +171,26 @@ int main(int argc, char* argv[])
     file.close();
 
     //read in name map
+    // first = true;
+    // std::string nameId, nameString;
+    // while (std::getline(key, line)) {
+
+    //     std::istringstream ss(line);
+        
+    //     // comma-separated file
+    //     std::getline(ss, nameId, ',');
+    //     std::getline(ss, nameString);
+
+    //     // this skips the csv header
+    //     if (first) {
+    //         first = false;
+    //         continue;
+    //     }
+
+    //     nameKey[std::stoi(nameId)] = nameString;
+    // }
+    std::unordered_map<std::string, int> nameKey;
+    std::unordered_map<int, std::string> idKey;
     first = true;
     std::string nameId, nameString;
     while (std::getline(key, line)) {
@@ -187,8 +207,25 @@ int main(int argc, char* argv[])
             continue;
         }
 
-        nameKey[std::stoi(nameId)] = nameString;
+        idKey[std::stoi(nameId)] = nameString;
+
+        if (!nameString.empty() && nameString.front() == '\"') 
+        {
+            nameString.erase(0, 1);
+        }
+        if (!nameString.empty() && nameString.back() == '\"') 
+        {
+            nameString.pop_back(); 
+        }
+
+        nameKey[nameString] = std::stoi(nameId);
     }
+
+    int startingURL = nameKey[start_url];
+    int endingURL = nameKey[end_url];
+
+    // std::cout << startingURL << std::endl;
+    // std::cout << endingURL << std::endl;
 
     // now call methods to traverse accordingly
     // std::cout << BFS_Search(startingURL, endingURL, adjacencyList).size() - 1 << std::endl;
@@ -202,7 +239,8 @@ int main(int argc, char* argv[])
 
     // Timer for BFS
     auto t1 = high_resolution_clock::now();
-    int bfs_result_size = BFS_Search(startingURL, endingURL, adjacencyList).size() - 1;
+    std::vector<int> bfs_results = BFS_Search(startingURL, endingURL, adjacencyList);
+    int bfs_result_size = bfs_results.size() - 1;
     auto t2 = high_resolution_clock::now();
     auto millis_bfs = duration_cast<milliseconds>(t2 - t1);
     auto seconds_bfs = duration_cast<seconds>(t2 - t1);
@@ -214,8 +252,8 @@ int main(int argc, char* argv[])
     auto millis_dfs = duration_cast<milliseconds>(t4 - t3);
     auto seconds_dfs = duration_cast<seconds>(t4 - t3);
 
-    std::cout << nameKey[startingURL] << std::endl;
-    std::cout << nameKey[endingURL] << std::endl;
+    std::cout << start_url << std::endl;
+    std::cout << end_url << std::endl;
 
     // Output results
     std::cout << bfs_result_size << std::endl;
@@ -226,6 +264,21 @@ int main(int argc, char* argv[])
     std::cout << seconds_dfs.count() << std::endl;
     std::cout << millis_dfs.count() << std::endl;
     // std::cout << dfs_duration_seconds << std::endl;
-
+    int counter = 0;
+    if (bfs_result_size <= 10)
+    {
+        for (const auto& path : bfs_results)
+        {
+            if (counter == bfs_result_size)
+            {
+                std::cout << idKey[path];
+            }
+            else 
+            {
+                std::cout << idKey[path] << " -> ";
+            }
+            counter++;
+        }
+    }
     return 0;
 }
